da1fce889d29454f20bd67f0b98e51aa8eea315a
Mary Margaret <mmlawton15@gmail.com>
2022-03-02 20:30:13 -0500

    finishing up 18.3.5

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -8,6 +8,26 @@ const $commentSection = document.querySelector('#comment-section');
 const $newCommentForm = document.querySelector('#new-comment-form');
 
 let pizzaId;
+function getPizza() {
+  //get id of pizza
+  const searchParams = new URLSearchParams(document.location.search.substring(1));
+  const pizzaId = searchParams.get('id');
+
+  //get pizzaInfo
+  fetch(`/api/pizzas/${pizzaId}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error({ message: 'Something went wrong!'});
+    }
+    return response.json();
+  })
+  .then(printPizza)
+  .catch(err => {
+    console.log(err);
+    alert('Cannot find a pizza with this idea, taking you back!');
+    window.history.back();
+  });
+}
 
 function printPizza(pizzaData) {
   console.log(pizzaData);
@@ -76,7 +96,7 @@ function printReply(reply) {
 `;
 }
 
-function handleNewCommentSubmit(event) {
+function handleNewCommentSubmit(event) { //THIS FUNCTION HAS WEIRD PARENTHESES
   event.preventDefault();
 
   const commentBody = $newCommentForm.querySelector('#comment').value;
@@ -87,6 +107,28 @@ function handleNewCommentSubmit(event) {
   }
 
   const formData = { commentBody, writtenBy };
+
+  fetch(`/api/comments/${pizzaId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 function handleNewReplySubmit(event) {
@@ -114,3 +156,6 @@ $backBtn.addEventListener('click', function() {
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
+
+
+getPizza();
